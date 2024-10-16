81aebb42161355a4f913cb61b2f1c838a52cfdcd
Gowrignaneswarreddy <gowrignaneswarreddy@gmail.com>
2024-10-16 19:12:20 +0530

    table

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Table from './pages/table'
+import "./App.css"
+const App = () => {
+  return (
+    <div>
+      <Table/>
+    </div>
+  )
+}
+
+export default App
